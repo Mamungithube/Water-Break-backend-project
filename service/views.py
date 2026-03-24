dfa4e2dce18c98a83e70a1a3f1d90bb56ec334b5
Mamungithube <orroshidmdmamun50@gmail.com>
2026-03-25 05:30:05 +0600

    permission set

--- service/views.py
+++ service/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
-from rest_framework.permissions import AllowAny, IsAuthenticated
-# Create your views here.
+from rest_framework.permissions import AllowAny
 from rest_framework import viewsets
 from .models import Privacy_Policy, TermsAndConditions, AboutUs
 from .serializers import PrivacyPolicySerializer, TermsAndConditionsSerializer, AboutUsSerializer
 
 
-class PrivacyPolicyViewSet(viewsets.ModelViewSet):
+class PrivacyPolicyViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [AllowAny]
     queryset = Privacy_Policy.objects.all().order_by('-updated_at')
     serializer_class = PrivacyPolicySerializer
 
 
-class TermsAndConditionsViewSet(viewsets.ModelViewSet):
+class TermsAndConditionsViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [AllowAny]
     queryset = TermsAndConditions.objects.all().order_by('-updated_at')
     serializer_class = TermsAndConditionsSerializer
 
 
-class AboutUsViewSet(viewsets.ModelViewSet):
+class AboutUsViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [AllowAny]
     queryset = AboutUs.objects.all().order_by('-updated_at')
     serializer_class = AboutUsSerializer
